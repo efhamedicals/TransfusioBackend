fd2cabf52dd31537ed9d4c8f13558476112d9a3b
Omar Farouk <Komarf28@gmail.com>
2026-03-11 04:12:48 +0000

    delete start

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-chmod -R 777 /app/public/avatars /app/public/fiches /app/public/patients
-chmod -R 775 /app/storage /app/bootstrap/cache
-
-(php-fpm -y /assets/php-fpm.conf &)
-nginx -c /assets/nginx.conf
